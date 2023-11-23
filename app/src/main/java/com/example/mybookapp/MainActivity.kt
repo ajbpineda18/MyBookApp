f774c7dbe53a4d1994fbf02372782c614a173be4
AJ Pineda <arnjcbpnd81801@gmail.com>
2023-11-23 21:03:45 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/mybookapp/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.mybookapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.mybookapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(getLayoutInflater())
+        setContentView(binding.root)
+
+        binding.btnBookList.setOnClickListener(this)
+        binding.btnFavoritesList.setOnClickListener(this)
+
+    }
+
+    override fun onClick(p0: View?) {
+        when(p0!!.id){
+            R.id.btn_book_list -> {
+                val intent = Intent(this, BookActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.btn_favorites_list -> {
+                val intent = Intent(this, FavoritesActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
+    }
+}
